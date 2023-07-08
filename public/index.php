1363d8ce3500622bfa22d63e5ec443eaf22787e8
Taytox <50372271+Taytox@users.noreply.github.com>
2023-07-08 22:45:20 +0100

    Generic error handler created,

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php   
 
 declare(strict_types=1);
-
-
+require dirname(__DIR__,1) . "/config/config.php";
+require dirname(__DIR__,1) . "/config/database.php";
+require dirname(__DIR__,1) . "/src/ErrorHandler.php";
 
 #Autoload required controller classes 
 spl_autoload_register(function ($class){
@@ -13,11 +14,13 @@ require dirname(__DIR__,1) . "/src/controller/$class.php";
 
 
 
+set_exception_handler("ErrorHandler::handleException");
+
+#Set the content type returned by the API to JSON
+header("Content-type:application/json; charset=UTF-8");
+
 
 $parts = explode("/",$_SERVER["REQUEST_URI"]);
-
-print_r($parts);
-
 
 if ($parts[1] != "workstreams"){
     http_response_code(404);
@@ -25,9 +28,9 @@ if ($parts[1] != "workstreams"){
 
 
 $id = $parts[2] ?? null;
-var_dump($id);
 
-
+$database = new database($config['DB_HOST'],$config['DB_DATABASE'],$config['DB_USERNAME'],$config['DB_PASSWORD']);
+$database->getConnection();
 
 $controller = new workstreamController;
 $controller -> processRequest($_SERVER["REQUEST_METHOD"],$id);
--- src/controller/WorkStreamController.php
+++ src/controller/WorkStreamController.php
@@ -4,8 +4,28 @@ class workstreamController
     public function processRequest(string $method, ?string $id):void 
     {
 
-        var_dump($method, $id);
+        #If an ID is in the URL, process for that single item otherwise process for a full collection. 
+        if ($id) {
+            
+           # $this->processResourceRequest($method, $id);
+            
+        } else {
+            
+            $this->processCollectionRequest($method);
+            
+        }
+    }
 
+
+    private function processCollectionRequest (String $method) : void
+    {
+        switch ($method) {
+            case "GET" : 
+                echo json_encode(["id" => 123]);
+                break;
+        }
+
+    
     }
     
 }
