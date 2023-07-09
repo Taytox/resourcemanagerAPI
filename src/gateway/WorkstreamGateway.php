3fac9396d6d7a8a5de199e1965bff28b57e9cdd7
Taytox <50372271+Taytox@users.noreply.github.com>
2023-07-09 22:53:56 +0100

    Workstream Basic Post Added.
    Added the ability to POST JSON to the API and create new workstreams.

--- src/gateway/WorkstreamGateway.php
+++ src/gateway/WorkstreamGateway.php
@@ -19,4 +19,22 @@ Class WorkstreamGateway{
         return $data;
 
     }
+
+    public function create(array $data) : string{
+
+        $sql = "INSERT INTO workstream(name,location,required_staffing,current_staffing)
+                VALUES (:name, :location, :required_staffing, :current_staffing)";
+
+
+        $stmt = $this->conn->prepare($sql);
+        echo "Name is" . $data["name"],
+        $stmt ->bindValue(":name", $data["name"], PDO::PARAM_STR);
+        $stmt ->bindValue(":location",$data["location"], PDO::PARAM_INT);
+        $stmt ->bindValue(":required_staffing",$data["reqstaffing"], PDO::PARAM_INT);
+        $stmt ->bindValue(":current_staffing",$data["curstaffing"] ?? 0, PDO::PARAM_INT);
+
+        $stmt ->execute(); 
+        return $this->conn->lastInsertId();
+    }
+
 }
