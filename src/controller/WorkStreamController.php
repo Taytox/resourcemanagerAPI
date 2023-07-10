23e49ceb3d75228a4f5b0dcd070e25ef89676921
Taytox <50372271+Taytox@users.noreply.github.com>
2023-07-10 23:34:41 +0100

    Workstream Patch + Delete added
    Added the handling of PATCH and DELETE requests for the workstream items.  Updated the validation to check for empty fields only when creating a new item and not when patching an existing one.

--- src/controller/WorkStreamController.php
+++ src/controller/WorkStreamController.php
@@ -15,7 +15,7 @@ class workstreamController
         #If an ID is in the URL, process for that single item otherwise process for a full collection. 
         if ($id) {
             
-           # $this->processResourceRequest($method, $id);
+            $this->processResourceRequest($method, $id);
             
         } else {
             
@@ -25,6 +25,54 @@ class workstreamController
     }
 
 
+    private function processResourceRequest(string $method, string $id):void
+    {
+        $workstream = $this->gateway->get($id);
+        if(! $workstream){
+            http_response_code(404);
+            echo json_encode(["Message"=> "Workstream not found"]);
+            return;
+        }
+
+        switch($method){
+            case "GET":
+                echo json_encode($workstream);
+            break;
+
+            case "PATCH":
+                $data = (array) json_decode(file_get_contents("php://input"),true);
+                
+                
+                #Carry out input validation
+                $errors = $this->getValidationErrors($data, false);
+                if (! empty($errors)){
+                    http_response_code(422);
+                    echo json_encode(["errors" => $errors]);
+                    break;
+                }
+                $rows = $this->gateway->update($workstream,$data);       
+                http_response_code(201);
+                echo json_encode([
+                    "message" => "Workstream $id updated",
+                    "rows" => $rows
+                ]);
+            break;
+            case "DELETE":
+                $rows = $this->gateway -> delete($id);
+                echo json_encode([
+                    "message" => "Workstream $id deleted",
+                    "rows" => $rows
+                ]);
+            break;
+            default:
+            http_response_code(405);
+            header("Allow: GET, PATCH,DELETE");
+        }
+        }
+
+        
+
+    }
     private function processCollectionRequest (String $method) : void
     {
         switch ($method) {
@@ -51,22 +99,23 @@ class workstreamController
                     "id" => $id
                 ]);
               break;
-            
-            
+            default:
+                http_response_code(405);
+                header("Allow: GET, POST");
             }
 
     }
 
-    private function getValidationErrors(array $data) : array
+    private function getValidationErrors(array $data, bool $new_entry = true) : array
     {
         $errors = [];
         
         #No Name entered
-        if (empty($data["name"])){
+        if ($new_entry && empty($data["name"])){
             $errors[] = "Workstream Name required";
         }
         #No location entered
-        if (empty($data["location"])){
+        if ($new_entry && empty($data["location"])){
             $errors[] = "location ID required";
             
         }#Location is not an Int representing a location ID
@@ -75,13 +124,13 @@ class workstreamController
         }    
     
         #No required staffing set
-        if (empty($data["reqstaffing"])){
-            $errors[] = "Required Staffing Must be set";
+       # if ($new_entry && empty($data["reqstaffing"])){
+         #  $errors[] = "Required Staffing Must be set";
             
-        }#required number of staff is not an Int representing a location ID
-        elseif(filter_var($data["reqstaffing"], FILTER_VALIDATE_INT)===false){
-                $errors[] = "Required Staffing must be an integer. ";
-        }    
+      #  }#required number of staff is not an Int 
+      #  elseif(filter_var($data["reqstaffing"], FILTER_VALIDATE_INT)===false){
+      #          $errors[] = "Required Staffing must be an integer. ";
+      #  }    
 
         return $errors;
 
--- src/gateway/WorkstreamGateway.php
+++ src/gateway/WorkstreamGateway.php
@@ -27,14 +27,58 @@ Class WorkstreamGateway{
 
 
         $stmt = $this->conn->prepare($sql);
-        echo "Name is" . $data["name"],
         $stmt ->bindValue(":name", $data["name"], PDO::PARAM_STR);
         $stmt ->bindValue(":location",$data["location"], PDO::PARAM_INT);
-        $stmt ->bindValue(":required_staffing",$data["reqstaffing"], PDO::PARAM_INT);
-        $stmt ->bindValue(":current_staffing",$data["curstaffing"] ?? 0, PDO::PARAM_INT);
+        $stmt ->bindValue(":required_staffing",$data["required_staffing"], PDO::PARAM_INT);
+        $stmt ->bindValue(":current_staffing",$data["current_staffing"] ?? 0, PDO::PARAM_INT);
 
         $stmt ->execute(); 
         return $this->conn->lastInsertId();
     }
 
+    public function get(string $id) : array | false
+    {
+        $sql = "SELECT * FROM workstream WHERE workstream_id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt -> bindValue(":id", $id, PDO::PARAM_INT);
+        $stmt ->execute();
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $data;
+    }
+
+    public function update(array $current, array $new): int
+    {
+        $sql = "UPDATE workstream 
+        SET name = :name, location = :location, required_staffing = :required_staffing, current_staffing = :current_staffing
+        WHERE workstream_id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt ->bindValue(":name", $new["name"] ?? $current["name"],PDO::PARAM_STR);
+        $stmt ->bindValue(":location", $new["location"] ?? $current["location"],PDO::PARAM_INT);
+        $stmt ->bindValue(":required_staffing", $new["required_staffing"] ?? $current["required_staffing"],PDO::PARAM_INT);
+        $stmt ->bindValue(":current_staffing", $new["current_staffing"] ?? $current["current_staffing"],PDO::PARAM_INT);
+        $stmt ->bindValue(":id",$current["workstream_id"], PDO::PARAM_INT);
+        $stmt -> execute();
+
+        return $stmt-> rowCount();
+        
+    }
+    public function delete(string $id): int
+    {
+        $sql = "DELETE FROM product
+                WHERE workstream_id = :id";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt -> bindValue(":id", $id,PDO::PARAM_INT);
+        $stmt -> execute();
+
+        return $stmt -> rowCount();
+
+
+
+
+
+
+
+    }
 }
