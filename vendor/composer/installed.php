b8c4a973442fa51d71a975ef2f94ec3b7409219c
Taytox <50372271+Taytox@users.noreply.github.com>
2023-07-18 10:37:23 +0100

    Composer updated
    Composer updated to generate composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fd7ff8a00ffbf62306df12eb736af6e6b49f6b82',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fd7ff8a00ffbf62306df12eb736af6e6b49f6b82',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
