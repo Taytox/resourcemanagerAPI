49778debeb35cbff1f839106ed8ec9fbe85cb6f4
Taytox <50372271+Taytox@users.noreply.github.com>
2023-07-28 15:34:54 +0100

    Fixed line placement

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php   
+declare(strict_types=1);
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-declare(strict_types=1);
 
 #if local dev enviroment, load config.php otherwise use environment vairables set in heroku. 
 if(is_file(dirname(__DIR__,1) . "/config/config.php")){
